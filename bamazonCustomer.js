5d0966e93dd89d68b2ef8d98eacd4a820c5a1cf0
jrm51c <jmccarthyn2n@yahoo.com>
2018-05-28 19:48:39 -0400

    added function to reject order if not enough stock or process order if enough stock exists, mimimum requirements complete

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,7 @@
 var mysql = require("mysql");
 var inquirerResponse = require('inquirer');
+var itemID;
+var quantityOrdered;
 
 // Connect to db
 var connection = mysql.createConnection({
@@ -16,41 +18,68 @@ var connection = mysql.createConnection({
   database: "bamazon"
 });
 
-connection.connect(function(err) {
-  if (err) throw err;
-  console.log("connected as id " + connection.threadId);
-  displayProducts();
-});
-
 // Query db and display products 
 function displayProducts() {
-  connection.query("SELECT item_id, product_name, price FROM products", function(err, res) {
+  connection.connect(function (err) {
     if (err) throw err;
-    console.log(res);
-    connection.end();
-    queryCustomer();
+    console.log("connected as id " + connection.threadId);
+  });
+  connection.query("SELECT item_id, product_name, price FROM products", function (err, res) {
+    if (err) throw err;
+    promptCustomer();
   });
 }
 
 // Prompt user for information
-function queryCustomer()    {
-    inquirerResponse.prompt([
-
-        {
-          type: "input",
-          name: "productID",
-          message: "Enter the product ID of the item you would like to purchase"
-        },
-        {
-          type: "input",
-          name: "quantity",
-          message: "How many units would you like to purchase?"
-        }
-      ]).then(function (inquirerResponse) {
-        var productID = inquirerResponse.productID;
-        var quantity = inquirerResponse.quantity;
-      });
-      console.log(productID);
-      console.log(quantity);
-
+function promptCustomer() {
+  inquirerResponse.prompt([
+    {
+      type: "input",
+      name: "itemID",
+      message: "Enter the ID of the item you would like to purchase:"
+    },
+    {
+      type: "input",
+      name: "quantity",
+      message: "How many units would you like to purchase?"
+    }
+  ]).then(function (inquirerResponse) {
+    itemID = inquirerResponse.itemID;
+    quantityOrdered = inquirerResponse.quantity;
+    determineStock();
+  });
 };
+
+
+// Query db to determine numer of units in stock 
+function determineStock() {
+  var numberInStock;
+
+  connection.query("SELECT stock_quantity FROM products WHERE item_id =" + itemID, function (err, res) {
+    if (err) throw err;
+    numberInStock = res[0].stock_quantity;
+    processOrder(numberInStock);
+  });
+};
+
+function processOrder(numberInStock) {
+  this.itemID = itemID;
+  this.quantityOrdered = quantityOrdered;
+  this.numberInStock = numberInStock;
+
+  if (this.quantityOrdered > this.numberInStock) {
+    console.log("Sorry, but we are unable to process your order at this time due to insufficient inventory.");
+    connection.end();
+  } else {
+    this.numberInStock -= this.quantityOrdered;
+    console.log(this.itemID);
+    console.log(this.quantityOrdered);
+    console.log(this.numberInStock);
+    connection.query("UPDATE products SET stock_quantity =" + this.numberInStock + "WHERE item_id =" + this.itemID, function (err, res) {
+      if (err) throw err;
+      connection.end();
+    });
+  }
+};
+
+displayProducts();
